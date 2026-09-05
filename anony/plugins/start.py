d0fafa275c4d613efc0fc69bda6c786f830ac596
isagzbyk46 <101513360+isagzbyk46@users.noreply.github.com>
2026-09-05 22:18:07 +0300

    Update start.py
    Signed-off-by: isagzbyk46 <101513360+isagzbyk46@users.noreply.github.com>

--- anony/plugins/start.py
+++ anony/plugins/start.py
@@ -55,7 +55,7 @@ async def start(_, message: types.Message):
         await db.add_chat(message.chat.id)
 
 
-@app.on_message(filters.command(["playmode", "settings"]) & filters.group & ~app.bl_users)
+@app.on_message(filters.command(["playmode", "settings", "ayarlar"]) & filters.group & ~app.bl_users)
 @lang.language()
 async def settings(_, message: types.Message):
     admin_only = await db.get_play_mode(message.chat.id)
